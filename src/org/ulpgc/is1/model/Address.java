5ad7cb166948ebe6752e88e1a2c66e7b31a34342
Paula <paulavelascosuarez6@gmail.com>
2024-10-18 18:21:50 +0100

    Clase Address Terminada

--- src/org/ulpgc/is1/model/Address.java
+++ src/org/ulpgc/is1/model/Address.java
@@ -1,4 +1,52 @@
 package org.ulpgc.is1.model;
 
 public class Address {
+    private String street;
+    private int number;
+    private String city;
+    private String postalCode;
+
+    public Address(String street, int number, String city, String postalCode) {
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return street + " " + number + ", " + city + " " + postalCode;
+    }
 }
