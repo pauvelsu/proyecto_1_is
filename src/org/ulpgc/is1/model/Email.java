772e2a43edb7a6d69ab6dd518a59e46cf35acc5d
Paula <paulavelascosuarez6@gmail.com>
2024-10-18 17:44:33 +0100

    Clase Email Terminada

--- src/org/ulpgc/is1/model/Email.java
+++ src/org/ulpgc/is1/model/Email.java
@@ -1,4 +1,34 @@
 package org.ulpgc.is1.model;
 
 public class Email {
+    private String address;
+
+    public Email(String address) {
+        if (isValidEmail(address)) {
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Invalid email format.");
+        }
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if (isValidEmail(address)) {
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Invalid email format.");
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.contains("@") && email.contains(".");
+    }
+
+    @Override
+    public String toString() {
+        return address;
+    }
 }
