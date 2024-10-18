aad0b707c4a8757a749b524a044b9a3436612185
Paula <paulavelascosuarez6@gmail.com>
2024-10-18 19:57:22 +0100

    Creación Main

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -1,4 +1,57 @@
 package org.ulpgc.is1.control;
 
-public class Main {
+import org.ulpgc.is1.model.Customer;
+import org.ulpgc.is1.model.Product;
+import org.ulpgc.is1.model.Purchase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Control {
+    private List<Customer> customers;
+    private List<Product>products;
+
+    public Control() {
+        customers = new ArrayList<>();
+        products = new ArrayList<>();
+    }
+    public boolean addCustomer(Customer customer) {
+        if (!customers.contains(customer)) {
+            customers.add(customer);
+            return true;
+        } else {
+            System.out.println("Customer already exists.");
+            return false;
+        }
+    }
+
+    public boolean addProduct(Product product) {
+        if (!products.contains(product)) {
+            products.add(product);
+            return true;
+        } else {
+            System.out.println("Product already exists.");
+            return false;
+        }
+    }
+    public boolean makePurchase(Customer customer, Product product) {
+        if (customers.contains(customer) && products.contains(product)) {
+            Purchase purchase = new Purchase(customer, product);
+            customer.addPurchase(purchase);
+            return true;
+        } else {
+            System.out.println("Either customer or product not found.");
+            return false;
+        }
+    }
+    public void showCustomerPurchases(Customer customer) {
+        if (customers.contains(customer)) {
+            List<Purchase> purchases = customer.getPurchases();
+            for (Purchase purchase : purchases) {
+                System.out.println(purchase);
+            }
+        } else {
+            System.out.println("Customer not found.");
+        }
+    }
 }
