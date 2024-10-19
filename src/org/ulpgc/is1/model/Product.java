17b20b3e2a351cd3ba381332d9ab092c0b875c63
tabera <tabera.albert.o@gmail.com>
2024-10-19 12:40:01 +0100

    Clase Product Terminada

--- src/org/ulpgc/is1/model/Product.java
+++ src/org/ulpgc/is1/model/Product.java
@@ -1,4 +1,52 @@
 package org.ulpgc.is1.model;
 
 public class Product {
+    private final int id;
+    private String name;
+    private String description;
+    private int price;
+    private ProductCategory category;
+
+    public Product(int id, String name, String description, int price, ProductCategory category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
 }
